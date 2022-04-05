4e80c9dfdb98fde4c97b8f4c1c9d8f93e1aee6eb
Borislav <borislav1207@gmail.com>
2022-04-05 18:54:34 +0300

    changed hardhat chainId to 31337 and transactions started working

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,6 +25,11 @@ module.exports = {
   solidity: "0.8.4",
   paths: {
     artifacts: "./src/artifacts",
-  }
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337
+    },
+  },
 };
  
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import OctopusBay from '../artifacts/contracts/Nft.sol/OctopusBay.json';
 import { getAccountPath } from "ethers/lib/utils";
 
 // адресът на solididity contract
-const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+const contractAddress = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0';
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 
