dc481cbb3d06d1484a7e96614ab8a3b8c6c6e6fc
Borislav <borislav1207@gmail.com>
2022-04-06 09:57:16 +0300

    ui changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,104 +1,15 @@
-import { WalletBalance } from "./WalletBalance";
 import { Navbar } from "./NavBar";
-import { useEffect, useState } from "react";
-import { ethers } from "ethers";
-import OctopusBay from '../artifacts/contracts/Nft.sol/OctopusBay.json';
-import { getAccountPath } from "ethers/lib/utils";
 import "../App.css";
-import "../css/Collection.css";
-// адресът на solididity contract
-const contractAddress = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0';
+import "../css/Home.css";
 
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-
-// крайният потребител
-const signer = provider.getSigner();
-
-const contract = new ethers.Contract(contractAddress, OctopusBay.abi, signer);
-
-export function Home() {
-
-  const [totalMinted, setTotalMinted] = useState(0);
-  useEffect(() => {
-    getCount();
-  }, []);
-
-  const getCount = async () => {
-    const count = await contract.count();
-    console.log(parseInt(count));
-    setTotalMinted(parseInt(count));
-  };
-
-  return (
-    <div>
-      <Navbar/>
-      <WalletBalance />
-
-      <h1>Octopus Bay NFT Collection</h1>
-      <div className="collection">
-        <div className="row">
-          {Array(totalMinted + 1)
-            .fill(0)
-            .map((_, i) => (
-              <div key={i} className="col">
-                <NFTImage tokenId={i} getCount={getCount} />
-              </div>
-            ))}
+export const Home = () => {
+    return (
+        <>
+        <Navbar/>
+        <div className="home">
+            <h1 className="home-mainText">Discover and mint extraordinary NFTs</h1>
+            <h3 className="home-secondaryText">Nftopus is the world's first and largest NFT marketplace </h3>
         </div>
-      </div>
-    </div>
-  );
+        </>
+    );
 }
-  
-  function NFTImage({ tokenId, getCount }) {
-    const contentId = 'QmajsqnX3VXeYAhdJP8VkB2wdmwusGEeMCvumurJJsCPou';
-    const metadataURI = `${contentId}/metadata/${tokenId}.json`;
-    const imageURI = `images/${tokenId}.png`;
-    
-      const [isMinted, setIsMinted] = useState(false);
-      useEffect(() => {
-        getMintedStatus();
-      }, [isMinted]);
-    
-      const getMintedStatus = async () => {
-        const result = await contract.isContentOwned(metadataURI);
-        console.log("We here", result)
-        setIsMinted(result);
-      };
-    
-      const mintToken = async () => {
-        const connection = contract.connect(signer);
-        const addr = connection.address;
-        const result = await contract.payToMint(addr, metadataURI, {
-          value: ethers.utils.parseEther('0.05'),
-        });
-    
-        await result.wait();
-        getMintedStatus();
-        getCount();
-      };
-    
-      async function getURI() {
-        const uri = await contract.tokenURI(tokenId);
-        alert(uri);
-      }
-      return (
-        <div className="collection-item">
-          <img className="collection-item-img" src={isMinted ? imageURI : 'images/placeholder.png'}></img>
-          <div className="collection-item-body">
-            <h5 className="collection-item__title">ID #{tokenId}</h5>
-            {!isMinted ? (
-              <button className="btn btn-primary" onClick={mintToken}>
-                Mint
-              </button>
-            ) : (
-              <button className="btn btn-secondary" onClick={getURI}>
-                Taken! Show URI
-              </button>
-            )}
-          </div>
-        </div>
-      );
-    }
-  
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,19 @@
-import "../css/Navbar.css";
+import "../css/NavBar.css";
 
 export const Navbar = () => {
+    const refreshPage = () => {
+        window.location.reload(false);
+      };
     return (
         <div className="navbar">
+            <div className="navbar-logo" onClick={refreshPage}>
+                <img src="images/LOGO.png" alt="logo" className="navbar-logo-img"/>
+                <p className="navbar-logo-text">Nftopus</p>
+            </div>
             <div className="navbar-item">Home</div>
-            <div className="navbar-item navbar-item--browse">Browse Collection</div>
+            <div className="navbar-item">Explore</div>
             <div className="navbar-item">About Us</div>
+            <div className="navbar-item">Wallet</div>
         </div>
     )
 }
